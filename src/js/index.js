a6d3caf8e3518c25947192e2aad99edf1bdb7ccd
shineyuer <shineying2012@163.com>
2017-01-12 17:46:03 +0800

    first

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+require('../css/index.css');
+require('../css/list.css');
+require('jquery');
+require('bootstrap/dist/css/bootstrap.css');
+require('./common.js');
+alert('hello 首页')
---
+++ src/js/list.js
@@ -0,0 +1,4 @@
+require('../css/list.css');
+require('bootstrap/dist/css/bootstrap.css');
+require('./common.js');
+alert('list 页')
